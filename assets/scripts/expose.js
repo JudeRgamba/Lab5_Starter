8e1d5fac2fcd2216ed4951099d5f8c78cb180ff5
Eric Nguyen <ern005@ucsd.edu>
2025-05-04 19:29:54 -0700

    Added Horn Select from dropdown for image and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,45 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let image = document.querySelector('img');
+  let audio = document.querySelector('audio');
+  const selectHorn = document.getElementById("horn-select");
+  selectHorn.addEventListener("change", function() {
+    const selectedValue = selectHorn.value;
+
+    if (selectedValue == 'air-horn') {
+      image.src = 'assets/images/air-horn.svg'; 
+      image.alt = 'Air Horn Selected';
+      audio.src = 'assets/audio/air-horn.mp3'; 
+      const playButton = document.querySelector('button');
+      playButton.addEventListener("click", (event) => { 
+        const audio = new Audio('assets/audio/air-horn.mp3');
+        audio.play();
+      });
+    }
+
+    else if (selectedValue == 'car-horn') {
+      image.src = 'assets/images/car-horn.svg'; 
+      image.alt = 'Car Horn Selected';
+      audio.src = 'assets/audio/car-horn.mp3'; 
+      const playButton = document.querySelector('button');
+      playButton.addEventListener("click", (event) => { 
+        const audio = new Audio('assets/audio/car-horn.mp3');
+        audio.play();
+      });
+    }
+
+    else if (selectedValue == 'party-horn') {
+      image.src = 'assets/images/party-horn.svg'; 
+      image.alt = 'Party Horn Selected';
+      audio.src = 'assets/audio/party-horn.mp3'; 
+      const playButton = document.querySelector('button');
+      playButton.addEventListener("click", (event) => { 
+        const audio = new Audio('assets/audio/party-horn.mp3');
+        audio.play();
+      });
+    }
+  });
+
+  
 }
